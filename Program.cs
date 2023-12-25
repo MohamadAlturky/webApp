dca0ba7cde68ceee9a3af71a6b45171fcca73149
mohamad.alturky <mohamad.alturky@hiast.edu.sy>
2023-12-25 10:25:32 +0300

    fix program

--- Program.cs
+++ Program.cs
@@ -9,12 +9,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
